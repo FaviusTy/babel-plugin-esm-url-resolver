3da86ce763f6eed6c45a1a32b128c35d09fce29f
Favi_ty <favius983@gmail.com>
2020-01-25 01:03:59 +0900

    test: more pattern

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = ({ types: t }) => {
   return {
     name: "esm-url-resolver",
     visitor: {
-      ImportDeclaration(path, state) {
+      ImportDeclaration(path) {
         const srcPath = path.node.source.value;
         if (srcPath[0] !== "." || extname(srcPath) !== "") return;
         path.node.source = t.stringLiteral(`${srcPath}.js`);
--- test/plugin.test.js
+++ test/plugin.test.js
@@ -7,14 +7,20 @@ pluginTester({
   tests: [
     {
       code: stripIndent`
-        import * as source from "./module1";
-        import { source2 } from "./module2.js";
+        import * as source1 from "./module1";
+        import * as source2 from "../module2";
+        import { source3 } from "./module3.js";
+        import { source4 } from "/module4";
+        import def, { sub1, sub2 } from "./module5/index";
         import lib from "lib";
         import { Component, render } from "https://cdn.pika.dev/preact/v8";
       `,
       output: stripIndent`
-        import * as source from "./module1.js";
-        import { source2 } from "./module2.js";
+        import * as source1 from "./module1.js";
+        import * as source2 from "../module2.js";
+        import { source3 } from "./module3.js";
+        import { source4 } from "/module4";
+        import def, { sub1, sub2 } from "./module5/index.js";
         import lib from "lib";
         import { Component, render } from "https://cdn.pika.dev/preact/v8";
       `
